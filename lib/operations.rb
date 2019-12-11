c216667af3942f60fef1de0814c464ee74de1770
George Blackmore <gtblackmore@gmail.com>
2019-12-11 03:40:44 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
 def unsafe?(speed)
-
+  if speed > 60
+    p true
+  elsif speed < 40
+    p true
+  else
+    p false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60 || speed < 40
+    p true
+  else speed >= 40 || speed <= 60 ? false : true
+  end
 end
 	
 
