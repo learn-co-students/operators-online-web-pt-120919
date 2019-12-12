8a7d42419e58a3d7be7c0e0b9956a8d6492ed687
christopher tate <chris29tate2@hotmail.co.uk>
2019-12-12 23:53:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+return  true if speed is > speed = 60 
+return false if speed is between = 40 & 50
+answer = 35 
 end
 
 
 
 def not_safe?(speed)
-	
+  return true if speed is > than 60
+  return false if speed is between 40 & 60
+  return if the speed is < than 40
+	answer = 79
 end
 	
 
