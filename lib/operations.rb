961b66e768bb3103bae9433e2399b774c3505394
Nguyen Ha <nynha0526@gmail.com>
2019-12-16 03:00:14 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
-
+  if speed == 40 < x < 60
+    return "unsafe"
+  else 
+    return "safe"
 end
 
-
-
 def not_safe?(speed)
-	
+	speed == 40 < x < 60? "unsafe" : "safe"
 end
 	
 
